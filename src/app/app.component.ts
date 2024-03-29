c337cf9df1816e7dcb46fc582ee674a75733917e
Cao Sỹ Hải Long <hailong@atlantic.edu.vn>
2024-03-29 14:33:30 +0700

    Create shooting feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'my-battleship';
+  tenArray = new Array(10);
+
+  onClickGameBox(event: MouseEvent) {
+    let gameBoxEl = event.target as HTMLDivElement;
+    let shotDotEl = gameBoxEl.querySelector('.shot-dot') as HTMLDivElement;
+    console.log(shotDotEl);
+    shotDotEl.style.display = 'block'
+  }
 }
